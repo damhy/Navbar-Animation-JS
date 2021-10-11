ba3f1dd6757f3a69ea61285dae3b27c404b55d9d
Your name heredamhy <oues.hamdy@gmail.com>
2021-10-11 02:58:11 -0700

    fiinal commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('.bubble');
+const gradients = [
+  "linear-gradient(to right, #e100ff,  #7f00ff)",
+  "linear-gradient(to right,#fdbb2d,#b21f1f,#1a2a6c)",
+  "linear-gradient(to right, #29ffc6, #20e3b2, #0cebeb)"
+];
+const options = {
+  threshold : 0.7
+}
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+  entries.forEach(entry => {
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    const gradientIndex = entry.target.getAttribute("data-index");
+    const coords = activeAnchor.getBoundingClientRect();
+    const directions = {
+      height: coords.height,
+      width: coords.width,
+      top: coords.top,
+      left: coords.left
+    }
+    if (entry.isIntersecting) {
+      bubble.style.setProperty('left',`${directions.left}px`)
+      bubble.style.setProperty('top',`${directions.top}px`)
+      bubble.style.setProperty('width',`${directions.width}px`)
+      bubble.style.setProperty('height',`${directions.height}px`)
+      bubble.style.background = gradients[gradientIndex]
+    }
+  })
+}
+
+sections.forEach(section => {
+  observer.observe(section)
+})
